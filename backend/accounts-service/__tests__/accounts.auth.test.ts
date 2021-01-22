928293aa3efa846a43b904a2d57297e32973f06f
monicavasco <monicavasco@gmail.com>
2021-01-22 19:18:05 -0300

    test: POST /accounts/login - 200 OK

--- backend/accounts-service/__tests__/accounts.auth.test.ts
+++ backend/accounts-service/__tests__/accounts.auth.test.ts
@@ -2,32 +2,32 @@ import request from 'supertest';
 import app from '../src/app';
 
 describe('Testando rotas de autenticação', () => {
-  // it('POST /accounts/login - 200 OK', async () => {
-  //   //mocking
-  //   const newAccount = {
-  //     id: 1,
-  //     name: 'Daniel',
-  //     email: 'danielcastro.rs@gmail.com',
-  //     password: '123456',
-  //   }
-  //   await request(app)
-  //     .post('/accounts/')
-  //     .send(newAccount)
+  it('POST /accounts/login - 200 OK', async () => {
+    //mocking
+    const newAccount = {
+      id: 1,
+      name: 'Daniel',
+      email: 'danielcastro.rs@gmail.com',
+      password: '123456',
+    }
+    await request(app)
+      .post('/accounts/')
+      .send(newAccount)
 
-  //   // testing
-  //   const payload = {
-  //     email: 'danielcastro.rs@gmail.com',
-  //     password: '123456'
-  //   }
+    // testing
+    const payload = {
+      email: 'danielcastro.rs@gmail.com',
+      password: '123456'
+    }
 
-  //   const resultado = await request(app)
-  //     .post('/accounts/login')
-  //     .send(payload);
+    const resultado = await request(app)
+      .post('/accounts/login')
+      .send(payload);
 
-  //     expect(resultado.status).toEqual(200);
-  //     expect(resultado.body.auth).toBeTruthy();
-  //     expect(resultado.body.token).toBeTruthy();
-  // })
+      expect(resultado.status).toEqual(200);
+      expect(resultado.body.auth).toBeTruthy();
+      expect(resultado.body.token).toBeTruthy();
+  })
 
   it('POST /accounts/login - 422 Unprocessable Entity', async () => {
     const payload = {
@@ -45,7 +45,7 @@ describe('Testando rotas de autenticação', () => {
   it('POST /accounts/login - 401 unauthorized', async () => {
     const payload = {
       email: 'danielcastro.rs@gmail.com',
-      password: 'abcabc'
+      password: 'abc123'
     }
 
     const resultado = await request(app)
